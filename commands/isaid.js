8677c8d7b48fa641fb70b0cac97f4c151d90ff4c
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-07 20:33:53 +0300

    Main

--- commands/isaid.js
+++ commands/isaid.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'isaid',
     description: "says hi",
     execute(message, args, cmd, client, Discord) {
-        const message = args[0]
+        const wantedmessage = args[0]
         if (!args.length){
             return message.channel.send('What would you like to say?')
         }
