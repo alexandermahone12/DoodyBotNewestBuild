b727f152fc35951f07ae91f9288296e764d5f50e
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-07 15:16:01 +0300

    Main

---
+++ commands/prefix.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    name: 'prefix',
+    aliases: ['changeme', 'removeslash'],
+    description: "creates an invite",
+    execute(message, args, cmd, client, Discord, prefix) {
+            if(args[0] === '!'){
+                prefix === '!'
+            }
+    }
+ 
+}
