0a70a934869ed8ae1c85e1c10e4b580399da33f4
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-08 15:52:40 +0300

    Main

--- commands/botinvitecode.js
+++ commands/botinvitecode.js
@@ -3,7 +3,11 @@ module.exports = {
     name: 'botinvite',
     description: "creates an invite for the bot",
     execute(message, args, cmd, client, Discord) {
-        message.channel.send('https://discord.com/oauth2/authorize?client_id=858809681518002187&scope=bot&permissions=8589934591');
+        const commandsEmbed = new Discord.MessageEmbed()
+        .setColor('#0000')
+        .setTitle('Click here to add DoodyBot to your server!')
+        .setLink('https://discord.com/oauth2/authorize?client_id=858809681518002187&scope=bot&permissions=8589934591')
+        message.channel.send(commandsEmbed);
 
     }
  
