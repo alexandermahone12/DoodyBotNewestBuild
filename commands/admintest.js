ed1e8de8b2f2c995a6fe9ddd7be7ff8dc3581c17
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-09 21:42:44 +0300

    main

--- commands/admintest.js
+++ commands/admintest.js
@@ -4,10 +4,10 @@ module.exports = {
     description: "Clear messages!",
     execute(message, args, cmd, client, Discord) {
         if(message.member.permissions.has("ADMINISTRATOR")){
-            message.channel.send("You have perms!")
+            message.reply("You have perms!")
     
     }else{
-        message.channel.send("You dont have admin perms!")
+        message.reply("You dont have admin perms!")
     }
 }
 }
