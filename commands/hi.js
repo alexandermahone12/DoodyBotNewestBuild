776046dfb3093595fc9514002af39ba2e6426d8d
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-09 20:17:54 +0300

    main

--- commands/hi.js
+++ commands/hi.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'hi',
     description: "says hi",
     execute(message, args, cmd, client, Discord) {
-        const commandsEmbed = new discord.MessageEmbed()
+        const commandsEmbed = new Discord.MessageEmbed()
         .setColor('#0000')
         .setDescription('Hello! This is [DoodyBot](https://doodybot2021.wixsite.com/doodybot)! To find out what i can do, use the command /commands.')
         message.channel.send(commandsEmbed);
