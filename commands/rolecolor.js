6bdf39f2679e1bcb08e4b36a0c24716a5eaacd0d
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-11 00:00:09 +0300

    wdada

--- commands/rolecolor.js
+++ commands/rolecolor.js
@@ -4,13 +4,13 @@ module.exports = {
     description: "Changes your color",
     execute(message, args, cmd, client, Discord) {
             const multilineString = `
-            <:redcolor:874703436241838202> <@&872060134833147906>
+            <:redcolor:874703436241838202> Red color
 
-            <:greencolor:874703023031586836> <@&874681708765651024>
+            <:greencolor:874703023031586836> Green color
 
-            <:purplecolor:874702566959775814> <@&874671480162635837>
+            <:purplecolor:874702566959775814> Purple color
 
-            <:pinkcolor:874702329767678063> <@&874682308421111848>
+            <:pinkcolor:874702329767678063> Pink color
             `;
             if (args[0] === 'init'){
                 const commandsEmbed = new Discord.MessageEmbed()
@@ -27,7 +27,19 @@ module.exports = {
                     return message.channel.send('Error: Red role not found, to fix this problem create a role name redcolor .')
                 }
                 message.member.roles.add(role)
-            }
+            }else if(args[0] === 'green'){
+                let role = message.guild.roles.cache.find(r => r.name === "greencolor")
+                if(!role){
+                    return message.channel.send('Error: green role not found, to fix this problem create a role name greencolor .')
+                }
+                message.member.roles.add(role)
 
-        }
+            }else if(args[0] === 'purple'){
+                let role = message.guild.roles.cache.find(r => r.name === "purplecolor")
+                if(!role){
+                    return message.channel.send('Error: Purple role not found, to fix this problem create a role name purplecolor .')
+                }
+                message.member.roles.add(role)   
+            }
     }
+}
