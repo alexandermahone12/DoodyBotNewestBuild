16fc1bded301715219c19b88893112079aa2ff76
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-10 21:28:51 +0300

    hgtfdahfgtdasa

--- commands/rolecolor.js
+++ commands/rolecolor.js
@@ -5,8 +5,11 @@ module.exports = {
     execute(message, args, cmd, client, Discord) {
             const multilineString = `
             <:redcolor:874703436241838202> : <@&872060134833147906>
+
             <:greencolor:874703023031586836> : <@&874681708765651024>
+
             <:purplecolor:874702566959775814> : <@&874671480162635837>
+            
             <:pinkcolor:874702329767678063> : <@&874682308421111848>`;
             if (args[0] === 'init'){
                 const commandsEmbed = new Discord.MessageEmbed()
