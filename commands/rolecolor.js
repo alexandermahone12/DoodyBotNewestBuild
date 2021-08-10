4350e59548cd136f702809836441dad1fa006d9a
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-10 21:35:02 +0300

    wdadd

--- commands/rolecolor.js
+++ commands/rolecolor.js
@@ -9,15 +9,21 @@ module.exports = {
             <:greencolor:874703023031586836> : <@&874681708765651024>
 
             <:purplecolor:874702566959775814> : <@&874671480162635837>
-            
+
             <:pinkcolor:874702329767678063> : <@&874682308421111848>`;
             if (args[0] === 'init'){
                 const commandsEmbed = new Discord.MessageEmbed()
-                .setColor('#0000')
+                .setColor('FADF2E')
                 .setTitle("List of colors")
                 .setDescription(multilineString)
                 .setFooter('DoodyBot version 1.1', 'https://i.postimg.cc/zftSmvqt/shadow.png');
-                message.channel.send(commandsEmbed);
+                message.channel.send(commandsEmbed).then((msg) =>{
+                    msg.react('<:redcolor:874703436241838202>');
+                    msg.react('<:greencolor:874703023031586836>');
+                    message.delete();
+                }).catch((err)=>{
+                    throw err;
+                });
 
             }
         }
