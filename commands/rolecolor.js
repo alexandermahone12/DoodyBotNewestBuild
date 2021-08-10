b350416d76a1e5967ec518575074591374210902
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-10 21:21:34 +0300

    dwadada

--- commands/rolecolor.js
+++ commands/rolecolor.js
@@ -8,12 +8,8 @@ module.exports = {
                 const commandsEmbed = new Discord.MessageEmbed()
                 .setColor('#0000')
                 .setTitle("List of colors")
-                .addFields(
-                    {name: "<:redcolor:874703436241838202> : <@&872060134833147906>", value: "\u200b"},
-                    {name: "<:greencolor:874703023031586836> : <@&874681708765651024>", value: "\u200b"},
-                    {name: "<:purplecolor:874702566959775814> : <@&874671480162635837>", value: "\u200b"},
-                    {name: "<:pinkcolor:874702329767678063> : <@&874682308421111848>", value: "\u200b"},
-                )
+                .setDescription("a")
+                .setDescription("a")
                 .setFooter('DoodyBot version 1.1', 'https://i.postimg.cc/zftSmvqt/shadow.png');
                 message.channel.send(commandsEmbed);
 
