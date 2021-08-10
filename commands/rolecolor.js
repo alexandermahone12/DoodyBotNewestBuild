67fa52a1db2846ca8db8e26ec4418d7804d490e5
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-10 23:30:50 +0300

    HeghEG

--- commands/rolecolor.js
+++ commands/rolecolor.js
@@ -12,49 +12,30 @@ module.exports = {
 
             <:pinkcolor:874702329767678063> <@&874682308421111848>
             `;
-            if (args[0] === 'init'){
-                const commandsEmbed = new Discord.MessageEmbed()
-                .setColor('FADF2E')
-                .setTitle("List of colors")
-                .setDescription(multilineString)
-                .setFooter('DoodyBot version 1.1', 'https://i.postimg.cc/zftSmvqt/shadow.png');
-                message.channel.send(commandsEmbed).then((msg) =>{
-                    msg.react('<:redcolor:874703436241838202>');
-                    msg.react('<:greencolor:874703023031586836>');
-                    message.delete();
-                    client.on("messageReactionAdd", (reaction, Member) => {
-                        if(Member.bot) return;
-                        reaction.message.reactions.cache.map(x=>{
-                            if(x._emoji.name != reaction._emoji.name&&x.users.cache.has(Member.id)) x.users.remove(Member.id)
-                        })
-                    });
-                    client.on('messageReactionAdd', async (reaction, user) => {
-                        if (reaction.partial) { //this whole section just checks if the reaction is partial
-                            try {
-                                await reaction.fetch(); //fetches reaction because not every reaction is stored in the cache
-                            } catch (error) {
-                                console.error('Fetching message failed: ', error);
-                                return;
-                            }
-                        }
-                        if (!user.bot) {
-                            if (reaction.emoji.id == "<:redcolor:874703436241838202>") { //if the user reacted with the right emoji
-                    
-                                const role = reaction.message.guild.roles.cache.find(r => r.name === "redcolor"); //finds role you want to assign (you could also user .name instead of .id)
-                    
-                                const { guild } = reaction.message //store the guild of the reaction in variable
-                    
-                                const member = guild.members.cache.find(member => member.id === user.id); //find the member who reacted (because user and member are seperate things)
-                    
-                                member.roles.add(role); //assign selected role to member
-                    
-                            }
-                        }
-                    })
-                }).catch((err)=>{
-                    throw err;
-                });
-
+            if(message.member.permissions.has("ADMINISTRATOR")){
+                if (args[0] === 'init'){
+                    const commandsEmbed = new Discord.MessageEmbed()
+                    .setColor('FADF2E')
+                    .setTitle("List of colors")
+                    .setDescription(multilineString)
+                    .setFooter('DoodyBot version 1.1', 'https://i.postimg.cc/zftSmvqt/shadow.png')
+                    message.channel.setDescription(commandsEmbed);
+    
+    
+                }
+            }else{
+                    const commandsEmbed = new Discord.MessageEmbed()
+                    .setColor('0000')
+                    .setDescription("You do not have the required permissions!")
+                    .setFooter('DoodyBot version 1.1', 'https://i.postimg.cc/zftSmvqt/shadow.png')
+                    message.channel.setDescription(commandsEmbed);
+            }
+            if(args[0] === 'red'){
+                if(!role){
+                    return message.channel.send('Error: Red role not found, to fix this problem create a role name redcolor')
+                }
+                let role = message.guild.roles.find(r => r.name === "redcolor");
+                member.roles.add(role)
             }
         }
     }
