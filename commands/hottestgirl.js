f165c6c33f80aace31545b0454961f82d548ee52
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-11 23:32:50 +0300

    drhfga

--- commands/hottestgirl.js
+++ commands/hottestgirl.js
@@ -3,30 +3,15 @@ module.exports = {
     name: 'whoisthehottest',
     description: "none",
     execute(message, args, cmd, client, Discord) {
-        const choiceone = 'Nobody'
-        const choose = Math.random(1-10)
-        if(choose === 1){
-            choiceone = 'Abdullah'
-        }else if(choose === 2){
-            choiceone = 'd'
-        }else if(choose === 3){
-            choiceone = 'f'
-        }else if(choose === 4){
-            choiceone = 'g'
-        }else if(choose === 5){
-            choiceone = 'f'
-        }else if(choose === 6){
-            choiceone = 'j'
-        }else if(choose === 7){
-            choiceone = 'k'
-        }else if(choose === 8){
-            choiceone = '5'
-        }else if(choose === 9){
-            choiceone = 'v'
-        }else if(choose === 10){
-            choiceone = 'aa'
-        }
-        message.channel.send(`The hottest person is ${choiceone}`);
+        var myArray = [
+            "Abdullah",
+            "No one",
+            "wdadadadd",
+            "dkawhbdkbad"
+          ];
+          
+        var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+        message.channel.send(`The hottest person is ${randomItem}`)
 
     }
  
