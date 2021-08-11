59eaa754e7bb42cdb352d80349fab9698d25eacd
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-11 23:36:17 +0300

    dddd

--- commands/hottestgirl.js
+++ commands/hottestgirl.js
@@ -6,12 +6,23 @@ module.exports = {
         var myArray = [
             "Abdullah",
             "No one",
-            "wdadadadd",
-            "dkawhbdkbad"
+            "Khalid",
+            "Catalina",
+            "Saba",
+            "Zahra",
+            "Janna",
+            "Mubarak",
+            "Shamouk",
+            "Moff",
+            "Ghamdan",
+            "Essa"
           ];
           
         var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
-        message.channel.send(`The hottest person is ${randomItem}`)
+        const commandsEmbed = new Discord.MessageEmbed()
+        .setColor('#0000')
+        .setDescription(`The hottest person is ${randomItem}`);
+        message.channel.send(commandsEmbed);
 
     }
  
