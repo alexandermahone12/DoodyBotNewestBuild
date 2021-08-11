de491fafae5720ed89cf29ecea24ef01ad257dd9
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-11 20:53:31 +0300

    htredsjtsjtgf

--- commands/verify.js
+++ commands/verify.js
@@ -3,11 +3,11 @@ module.exports = {
     name: 'verify',
     description: "None",
     execute(message, args, cmd, client, Discord) {
-        if (message.channel.id === "875059824457560154") {
-            var role = message.member.roles.cache.find(role => role.name === "verified");
-            if (!role) return;
-            message.member.guild.roles.add(role);
-            message.reply('Youve been verified!')
-        }
+
+        var role = message.member.roles.cache.find(role => role.name === "verified");
+        if (!role) return;
+        message.member.guild.roles.add(role);
+        message.reply('Youve been verified!')
+        
     }
 }
