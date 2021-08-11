8f31ebf15035f393e102d886fd43f7ff4d8c904f
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-11 20:47:46 +0300

    BFBDB

--- commands/verify.js
+++ commands/verify.js
@@ -4,14 +4,10 @@ module.exports = {
     description: "None",
     execute(message, args, cmd, client, Discord) {
 
-        var Channel = message.channel.name
-        if(Channel != "𝙫𝙚𝙧𝙞𝙛𝙞𝙘𝙖𝙩𝙞𝙤𝙣-𝙘𝙝𝙖𝙣𝙣𝙚𝙡") {
-            return message.reply('You cannot use this command here');
-        } else {
-            var role = message.member.guild.roles.cache.find(role => role.name === "verified");
-            message.member.guild.roles.add(role);
-            message.reply('Youve been verified!')
-        }
+        var role = message.member.roles.cache.find(role => role.name === "verified");
+        message.member.guild.roles.add(role);
+        message.reply('Youve been verified!')
+        
 
         
     }
