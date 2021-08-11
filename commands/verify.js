a8d6bcff4de7d834211219fb82e98fb13392a873
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-11 21:15:33 +0300

    dwad

--- commands/verify.js
+++ commands/verify.js
@@ -3,9 +3,13 @@ module.exports = {
     name: 'verify',
     description: "None",
     execute(message, args, cmd, client, Discord) {
+        if(message.member.roles.cache.has('874736823488901180')){
+            message.reply("You are already verified! Please don't spam this command.")
+        }else{
+            message.member.roles.add('874736823488901180');
+            message.reply('Youve been verified!')
+        }
 
-        message.member.roles.add('874736823488901180');
-        message.reply('Youve been verified!')
         
     }
 }
