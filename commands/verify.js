7ec4347af2e656ff2b3c587f0451668b51ec3c51
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-11 21:31:27 +0300

    dwadad

--- commands/verify.js
+++ commands/verify.js
@@ -3,13 +3,17 @@ module.exports = {
     name: 'verify',
     description: "None",
     execute(message, args, cmd, client, Discord) {
+        if(message.channel.id === "875059824457560154"){
         if(message.member.roles.cache.has('874736823488901180')){
             message.reply("You are already verified! Please don't spam this command.")
         }else{
             message.member.roles.add('874736823488901180');
             message.reply('Youve been verified!')
         }
-
+    }else{
+        message.reply("You can only use this command in Doody's server / verify channel.")
+    }
         
+
     }
 }
