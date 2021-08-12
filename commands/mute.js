f435195e269026bde954f06641edbf7e2635541c
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-12 17:07:35 +0300

    Coding a catch error code so it doesnt crash

--- commands/mute.js
+++ commands/mute.js
@@ -10,6 +10,20 @@ module.exports = {
     
                 let mainRole = message.guild.roles.cache.find(role => role.name === 'Member');
                 let muteRole = message.guild.roles.cache.find(role => role.name === 'mute');
+                if (!mainRole){
+                    const commandsEmbed4 = new Discord.MessageEmbed()
+                    .setColor('#0000')
+                    .setDescription("Error: Member role wasn't created.")
+                    .setFooter('DoodyBot version 1.2', 'https://i.postimg.cc/zftSmvqt/shadow.png');
+                    return message.channel.send(commandsEmbed4)
+                }else if (!muteRole){
+                    const commandsEmbed5 = new Discord.MessageEmbed()
+                    .setColor('#0000')
+                    .setDescription("Error: mute role wasn't created.")
+                    .setFooter('DoodyBot version 1.2', 'https://i.postimg.cc/zftSmvqt/shadow.png');
+                    return message.channel.send(commandsEmbed5)
+                }
+
     
                 let memberTarget = message.guild.members.cache.get(target.id);
     
