0ae9fcc2f4f38437b41f6e236c941c2bf7d84f15
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-12 19:01:25 +0300

    Server info command

---
+++ commands/serverinfo.js
@@ -0,0 +1,23 @@
+
+module.exports = {
+
+    name: 'serverinfo',
+    description: "says hi",
+    execute(message, args, cmd, client, Discord) {
+        const ServerLogo = message.guild.iconURL();
+        const ServerInfoEmbed = new Discord.MessageEmbed()
+            .setColor('#b700ff')
+            .setTitle("Server Info")
+            .setImage(ServerLogo)
+            .setDescription(`About **${message.guild}**`)
+            .addField("**Date Created**", `Server Created on **${message.guild.createdAt.toLocaleString()}**`)
+            .addField("**Owner**", `The Owner of This Server is ${message.guild.owner}`)
+            .addField("**Member Count**", "This Server Has ` " + `${message.guild.memberCount}` + " ` **Members**")
+            .addField("**Emoji Count**", "This Server Has ` " + `${message.guild.emojis.cache.size}` + " ` **Emojis**")
+            .addField("**Roles Count**", "This Server Has ` " + `${message.guild.roles.cache.size}` + " ` **Roles**")
+            .addField("**Channels Count**", "This Server Has ` " + `${message.guild.channels.cache.size}` + " ` **Channels**")
+            .addField("**Server Logo**", "**Downlaod Server Logo**")
+            .setURL(ServerLogo)
+            message.channel.send(ServerInfoEmbed)
+          }    
+        }
--- commands/suggest.js
+++ commands/suggest.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'creates a suggestion!',
     execute(message, args, cmd, client, Discord){
         const channel = message.guild.channels.cache.find(c => c.id === '875404469360295947');
-        if(!channel) return message.channel.send('suggestions channel does not exist!, to solve this create a channel named〖❓〗Suggestions');
+        if(!channel) return message.channel.send('This command can only be used in the public server.');
 
         let messageArgs = args.join(' ');
         const embed = new Discord.MessageEmbed()
