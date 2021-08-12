7a84d7b9b7f299649b4a88d155bd47c33a688a0c
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-13 01:28:45 +0300

    Report command v1

---
+++ commands/report.js
@@ -0,0 +1,33 @@
+
+module.exports = {
+
+    name: 'report',
+    description: "reports the mentioned person",
+    execute(message, args, cmd, client, Discord) {
+        const target = message.mentions.users.first();
+        let memberTarget = message.guild.members.cache.get(target.id);
+        if (target){
+
+        
+        if (!args[1]){
+            return message.channel.send("What do you want to report the person for?")
+        }else{
+            const channel = message.guild.channels.cache.find(c => c.id === '874348107314384996');
+            if (!channel){
+                message.channel.send("Channel doesnt exist")
+            }
+            const embed = new Discord.MessageEmbed()
+            .setColor("0000")
+            .setDescription(`<@${memberTarget.user.id}> has been reported for ${args[1]}`)
+            channel.send(embed)
+            
+        }
+
+
+
+    }else{
+        message.channel.send("Please mention the person you want to report.")
+    }
+}
+ 
+}
