1bf83ad5b853336a05e6768e43e59dd5bd1c9d93
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-13 01:34:33 +0300

    ddd

--- commands/report.js
+++ commands/report.js
@@ -5,13 +5,13 @@ module.exports = {
     description: "reports the mentioned person",
     execute(message, args, cmd, client, Discord) {
         const target = message.mentions.users.first();
-        let memberTarget = message.guild.members.cache.get(target.id);
         if (target){
 
         
-        if (!args[1]){
-            return message.channel.send("What do you want to report the person for?")
+            if (!args[1]){
+                return message.channel.send("What do you want to report the person for?")
         }else{
+            let memberTarget = message.guild.members.cache.get(target.id);
             const channel = message.guild.channels.cache.find(c => c.id === '874348107314384996');
             if (!channel){
                 message.channel.send("Channel doesnt exist")
