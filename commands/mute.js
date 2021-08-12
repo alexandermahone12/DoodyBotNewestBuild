36c2bab4b26916b1298fb0079eee529c5023d411
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-12 05:59:14 +0300

    Mute&Unmute commands

---
+++ commands/mute.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+    name: 'mute',
+    description: "creates an invite for the bot",
+    execute(message, args, cmd, client, Discord) {
+        const commandsEmbed = new Discord.MessageEmbed()
+        .setColor('#0000')
+        .setTitle('Add DoodyBot to your server!')
+        .setDescription('[Click here!](https://discord.com/oauth2/authorize?client_id=858809681518002187&scope=bot&permissions=8589934591)')
+        .addFields({ name: "Important", value: "When adding doodybot, it will ask you to authorize some permissions. You do not have to tick all the boxes.", inline: true })
+        .setFooter('DoodyBot version 1.2', 'https://i.postimg.cc/zftSmvqt/shadow.png');
+        message.channel.send(commandsEmbed);
+
+    }
+ 
+}
---
+++ commands/unmute.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+    name: 'unmute',
+    description: "None",
+    execute(message, args, cmd, client, Discord) {
+        const target = message.mentions.users.first();
+        if(target){
+            let mainRole = message.guild.roles.cache.find(role => role.name === 'Member');
+            let muteRole = message.guild.roles.cache.find(role => role.name === 'mute');
+ 
+            let memberTarget= message.guild.members.cache.get(target.id);
+ 
+            memberTarget.roles.remove(muteRole.id);
+            memberTarget.roles.add(mainRole.id);
+            message.channel.send(`<@${memberTarget.user.id}> has been unmuted`);
+        } else{
+            message.channel.send('Cant find that member!');
+        }
+    }
+}
+
+
