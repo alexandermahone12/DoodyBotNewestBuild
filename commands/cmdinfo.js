32a94305381775ccda09d50676bd81fd39e022a8
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-16 02:02:10 +0300

    cmdinfo command

---
+++ commands/cmdinfo.js
@@ -0,0 +1,37 @@
+module.exports = {
+
+    name: 'info',
+    description: "Info about certain command",
+    execute(message, args, cmd, client, Discord) {
+        if (message.channel instanceof Discord.DMChannel){
+            return message.channel.send("You cannot use this command in DMs")
+        }else{
+            if (!args.length){
+                return message.channel.send("The info command shows you info about a certain command, type the command name as an arguement. If you dont know what the doodybotcommands are, use !info list.")
+            }else if(args[0] === 'list'){
+                var Myarray = Array = [
+                    "admintest",
+                    "avatar",
+                    "botinvite",
+                    "clear",
+                    "hi",
+                    "lock",
+                    "unlock",
+                    "mute",
+                    "unmute",
+                    "report",
+                    "serverinfo",
+                    "status",
+                    "suggest",
+                    "tempchat",
+                    "tempvoice",
+                    "verify",
+                    "whoami",
+                    "ytg"
+                ]
+                message.channel.send(Myarray);
+            }
+        }
+    }
+ 
+}
