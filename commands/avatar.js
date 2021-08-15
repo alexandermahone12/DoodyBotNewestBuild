e0127fbf742b6aebc2c94a9147baea18fbb255a9
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-15 23:27:04 +0300

    Download link

--- commands/avatar.js
+++ commands/avatar.js
@@ -11,13 +11,13 @@ module.exports = {
             let member = message.mentions.users.first() || message.author
 
             let avatar = member.displayAvatarURL({dynamic : true, size: 1024})
-
+            let download = member.displayAvatarURL({size: 1024})
 
             const embed = new Discord.MessageEmbed()
             .setTitle("Avatar command!")
             .setDescription(`${member.username}'s avatar`)
             .setImage(avatar)
-            .setFooter(`[Download](${member.displayAvatarURL({size: 1024})}) ${member.username}'s avatar`)
+            .setFooter(`[Download](${download}) ${member.username}'s avatar`)
             .setColor("RANDOM")
 
             message.channel.send(embed);
