dce493ec7a00fbe22befff6cde24afd4946c2227
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-16 00:54:42 +0300

    WHOAMI COMMAND

---
+++ commands/whoami.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+    name: 'avatar',
+    description: "shows the profile pic",
+    execute(message, args, cmd, client, Discord) {
+        if (message.channel instanceof Discord.DMChannel){
+            return message.channel.send("You cannot use this command in DMs")
+        }else{
+            const embed = new Discord.MessageEmbed()
+            .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+            .setDescription(`You are ${message.author.tag}, you joined ${message.guild} in ${message.guild.member.joinedAt}, you have the following roles: ${message.guild.member.roles}`)
+            .setImage(avatar)
+            .setColor("RANDOM")
+
+            message.channel.send(embed);
+        }
+    }
+ 
+}
