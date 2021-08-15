f3cf2e05f5f1eff6b30ed3d20bf249255e944060
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-15 04:21:05 +0300

    hhh

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,4 +1,3 @@
-const { MessageButton } = require("discord-buttons");
 
 module.exports = {
 
@@ -13,17 +12,12 @@ module.exports = {
 
             let avatar = member.displayAvatarURL({dynamic : true, size: 1024})
 
-            let button = new MessageButton()
-                .setStyle('red')
-                .setLabel('My First Button!') 
-                .setID('click_to_function');
 
-            message.channel.send('Hey, i am powered by https://npmjs.com/discord-buttons', button);
             const embed = new Discord.MessageEmbed()
             .setTitle(`${member.username}'s avatar`)
             .setImage(avatar)
             .setColor("RANDOM")
-            
+
             message.channel.send(embed);
         }
     }
