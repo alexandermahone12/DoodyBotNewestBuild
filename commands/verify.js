17f2ede3a015ad78c76b54593870bd47e017bb8e
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-16 05:16:44 +0300

    fd

--- commands/verify.js
+++ commands/verify.js
@@ -1,3 +1,4 @@
+const fs = require('fs') 
 module.exports = {
 
     name: 'verify',
@@ -12,7 +13,17 @@ module.exports = {
             }else{
                 message.member.roles.remove('875704950804578304');
                 message.member.roles.add('874736823488901180');
-                message.reply('Youve been verified!')
+                const replytouser = message.reply('Youve been verified!')
+  
+                // Data which will write in a file. 
+                let data = `${replytouser}`
+                
+                // Write data in 'Output.txt' . 
+                fs.writeFile('C:\Users\ale48\Desktop\Programming\Javascript\SERVER\DoodyBotNewestBuild\assets\verifyLOG.txt', data, (err) => { 
+                    
+                    // In case of a error throw err. 
+                    if (err) throw err; 
+                }) 
             }
         }else{
             message.reply("You can only use this command in Doody's server / verify channel.")
