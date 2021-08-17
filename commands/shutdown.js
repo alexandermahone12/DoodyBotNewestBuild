6cdac19078af3772b74abb82a0078c3f9cf80ef9
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-17 04:24:03 +0300

    shutdown command

---
+++ commands/shutdown.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: 'shutdown',
+    description: "creates a temp chat",
+    execute(message, args, cmd, client, Discord) {
+        if (message.channel instanceof Discord.DMChannel){
+            return message.channel.send("You cannot use this command in DMs")
+        }else{
+            if (!args.length){
+                message.channel.send("Please enter the password.")
+            }
+            if (args[0]==='Abooodi1212'){
+                console.log("SHUTDOWN MODE")
+                message.channel.send("Shutting down...").then(() => {
+                client.destroy();
+            })
+        }else{
+            console.log("INCORRECT PASSWORD!")
+            message.channel.send("Password incorrect. reporting to server.")
+        }
+        }
+    }
+}
