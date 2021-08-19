a4d34eb710f00e3cc9d2c363df1ea57d2bda001c
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-19 11:58:16 +0300

    Setting up coin system

---
+++ events/guild/guildMemberAdd.js
@@ -0,0 +1,11 @@
+const profileModel = require('../../models/profileSchema')
+
+module.exports = async(Discord, client, message) =>{
+    let profile = await profileModel.create({
+        userID: message.member.id,
+        serverID: message.guild.id,
+        coins: 1000,
+        bank: 0
+    });
+    profile.save();
+}
