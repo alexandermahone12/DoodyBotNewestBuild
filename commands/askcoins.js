098e35622383f29a32d5c05a576943333d8976c8
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-19 13:35:05 +0300

    OH NOOOO

--- commands/askcoins.js
+++ commands/askcoins.js
@@ -3,11 +3,11 @@ module.exports = {
     name: "ask",
     description: "Asks for coins",
     async execute(message, args, cmd, client, Discord, profiledata){
+        const randomNumber = Math.floor(Math.random() * 500) + 1;
         const commandsEmbed1 = new Discord.MessageEmbed()
             .setColor('#554846')
-            .setDescription(`You've been given ${response} coins!`)
+            .setDescription(`You've been given ${randomNumber} coins!`)
             .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
-        const randomNumber = Math.floor(Math.random() * 500) + 1;
         const response = await profileModel.findOneAndUpdate(
         {
             userID: message.author.id,
