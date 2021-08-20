ee39eaa6afaf89b17f5e558343e523c7e9b1055b
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-20 04:23:01 +0300

    a

--- events/guild/message.js
+++ events/guild/message.js
@@ -24,7 +24,7 @@ module.exports = async (Discord, client, message) => {
     const cmd = args.shift().toLowerCase();
 
     const command = client.commands.get(cmd) || client.commands.find(a => a.aliases && a.aliases.includes(cmd));
-    if (command.name === 'a') return message.channel.send("Command doesnt exist");
+    if (!command) return message.channel.send("Command doesnt exist");
 
     if(!cooldowns.has(command.name)){
         cooldowns.set(command.name, new Discord.Collection());
