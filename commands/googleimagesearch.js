7296d95099e8760b635920b311280f1db5d2ddd2
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-20 13:57:30 +0300

    Imagennanana

--- commands/googleimagesearch.js
+++ commands/googleimagesearch.js
@@ -1,5 +1,4 @@
-var cheerio = require('cheerio');
-var request = require('request');
+const GoogleImages = require('google-images');
 module.exports = {
     name: "search",
     description: "Generates a google image based on keyword",
@@ -8,41 +7,32 @@ module.exports = {
         if (message.channel instanceof Discord.DMChannel){
             return message.channel.send("You cannot use this command in DMs")
         }else{
-            var search = args.join(' ');
-    
-            var options = {
-                url: "http://results.dogpile.com/serp?qc=images&q=" + search,
-                method: "GET",
-                headers: {
-                    "Accept": "text/html",
-                    "User-Agent": "Chrome"
-                }
-            };
-            request(options, function(error, response, responseBody) {
-                if (error) {
-                    // handle error
-                    return;
-                }
-        
-                /* Extract image URLs from responseBody using cheerio */
-        
-                $ = cheerio.load(responseBody); // load responseBody into cheerio (jQuery)
-        
-                // In this search engine they use ".image a.link" as their css selector for image links
-                var links = $(".image a.link");
-        
-                // We want to fetch the URLs not the DOM nodes, we do this with jQuery's .attr() function
-                // this line might be hard to understand but it goes thru all the links (DOM) and stores each url in an array called urls
-                var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
-                console.log(urls);
-                if (!urls.length) {
-                    // Handle no results
-                    return;
-                }
-        
-                // Send result
-                message.channel.send( urls[0] );
-        });
+ 
+            const client = new GoogleImages('1d2a5f5649173afb2', 'AIzaSyCNrPr9riXoc9pFQrrSG52FLn2MHqMnoj0');
+             
+            client.search('Steve Angello')
+                .then(images => {
+                    /*
+                    [{
+                        "url": "http://steveangello.com/boss.jpg",
+                        "type": "image/jpeg",
+                        "width": 1024,
+                        "height": 768,
+                        "size": 102451,
+                        "thumbnail": {
+                            "url": "http://steveangello.com/thumbnail.jpg",
+                            "width": 512,
+                            "height": 512
+                        }
+                    }]
+                     */
+                });
+             
+            // paginate results
+            client.search('Steve Angello', {page: 2});
+             
+            // search for certain size
+            client.search('Steve Angello', {size: 'large'});
  
 
 
