81b48d9647c575abf1970efbfdd0bc61b0efd45d
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-20 08:25:01 +0300

    AAA

--- commands/meme.js
+++ commands/meme.js
@@ -3,20 +3,13 @@ module.exports = {
     description: "Creates account in database",
     execute(message, args, cmd, client, Discord, profiledata){
         const { reddit } = require("reddit.images");
-        reddit.FetchRandomMeme("top").then((data) => {
+        reddit.FetchRandomMeme("hot").then((data) => {
             const commandsEmbed1 = new Discord.MessageEmbed()
             .setColor('#554846')
             .setDescription(`[${data.title}](${data.postLink})`)
             .setImage(data.image)
             .setFooter(`👍${data.upvotes} || 👎${data.downvotes}`)
-            const thumbsup = '👍';
-            const thumbsdown = '👎';
-            message.channel.send(commandsEmbed1).then(embedMsg => {
-                embedMsg.react(thumbsup)
-                embedMsg.react(thumbsdown)
-            }).catch((err)=>{
-                console.log(err)
-            })
+            message.channel.send(commandsEmbed1)
         });   
     }
 }
