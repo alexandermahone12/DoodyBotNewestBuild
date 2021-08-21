ad63681af3cc033e4f97b58c7ac582528a150e07
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-21 15:45:36 +0300

    Maybe

--- commands/tasksforcoins.js
+++ commands/tasksforcoins.js
@@ -1,3 +1,4 @@
+const problems = require('maths-problems')
 module.exports = {
     name: "task",
     description: "Generates a mathematical problem",
@@ -6,37 +7,44 @@ module.exports = {
         if (message.channel instanceof Discord.DMChannel){
             return message.channel.send("You cannot use this command in DMs")
         }else{
-            const questions = [
-                "What is 2+2"
-            ]
-            let counter = 0
-            const filter = m => m.author.id === message.author.id
-            const collector = new Discord.MessageCollector(message.channel, filter, {
-                max: questions.length,
-                time: 1000 * 5,
-            })
-
-            message.channel.send(questions[counter++])
-            collector.on('collect', m => {
-                if (counter < questions.length){
-                    m.channel.send(questions[counter++])
-                }
-            })
-
-            collector.on('end', collected => {
-                console.log(`Collected ${collected.size} messages!`)
-
+            if (!args.length){
+                message.channel.send("What kind of game would you like to play?")
+            }
+            if (args[0] === 'math'){
+                const question = problems.generateQuestions([additionProblem, subtractionProblem], [5,5])
+                var result = problems.markQuestion(question, additionProblem)
+                const questions = [
+                    `What is ${question}`
+                ]
                 let counter = 0
-                collected.forEach((value) => {
-                    console.log(questions[counter++], value.content)
-                    if (value.content === '4'){
-                        return message.channel.send("Correct!")
-                    }else{
-                        message.channel.send("Incorrect!")
+                const filter = m => m.author.id === message.author.id
+                const collector = new Discord.MessageCollector(message.channel, filter, {
+                    max: questions.length,
+                    time: 1000 * 5,
+                })
+
+                message.channel.send(questions[counter++])
+                collector.on('collect', m => {
+                    if (counter < questions.length){
+                        m.channel.send(questions[counter++])
                     }
                 })
-            })
 
+                collector.on('end', collected => {
+                    console.log(`Collected ${collected.size} messages!`)
+
+                    let counter = 0
+                    collected.forEach((value) => {
+                        console.log(questions[counter++], value.content)
+                        if (value.content === result){
+                            message.channel.send("Correct!")
+                            return
+                        }else{
+                            message.channel.send("Incorrect!")
+                        }
+                    })
+                })
+            }
         }
     }
 }
