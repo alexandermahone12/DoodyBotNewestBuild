51bff211e511c264e9c60ea3ea0d6f4104fa8b1e
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-21 19:06:58 +0300

    Shop first try

--- commands/buyusingcoins.js
+++ commands/buyusingcoins.js
@@ -1,3 +1,4 @@
+const profileModel = require('../models/profileSchema')
 
 module.exports = {
     name: "buy",
@@ -7,26 +8,29 @@ module.exports = {
         if (message.channel instanceof Discord.DMChannel){
             return message.channel.send("You cannot use this command in DMs")
         }else{
+            var StickersPermsPrice = 1000000;
+            var EmojiPermsEmojiPrice = 1000000;
             const multilineString = `
             React to an emoji to buy the wanted item
 
+            🖼️<@&878667764716408842> Price: ${StickersPermsPrice} Coins
 
-            
+            🤑<@&878667541042569227> Price: ${EmojiPermsEmojiPrice} Coins
+
             `
-            const item1 = '';
-            var item1price = 100000;
-            const item2 = '';
-            var item2price = 100000;
+            const StickersPermsEmoji = '🖼️';
+            const EmojiPermsEmoji = '🤑';
+
 
             const channel = '878119198558134292';
             let embed = new Discord.MessageEmbed()
                 .setColor('#554846')
-                .setTitle('Coins shop!')
+                .setTitle("Realm's shop!")
                 .setDescription(multilineString)
                 .setFooter('To suggest a new item use !suggest');
             let messageEmbed = await message.channel.send(embed);
-            messageEmbed.react(item1);
-            messageEmbed.react(item2);
+            messageEmbed.react(StickersPermsEmoji);
+            messageEmbed.react(EmojiPermsEmoji);
             client.on('messageReactionAdd', async (reaction, user) => {
                 if (reaction.message.partial) await reaction.message.fetch();
                 if (reaction.partial) await reaction.fetch();
@@ -34,10 +38,36 @@ module.exports = {
                 if (!reaction.message.guild) return;
      
                 if (reaction.message.channel.id == channel) {
-                    if (reaction.emoji.name === item1) {
-                        await reaction.message.author.send('1')
+                    if (reaction.emoji.name === StickersPermsEmoji) {
+                        if(profiledata.coins < StickersPermsPrice){
+                            const response = await profileModel.findOneAndUpdate(
+                                {
+                                    userID: message.author.id,
+                                }, 
+                                {
+                                    $inc: {
+                                        coins: -StickersPermsPrice,
+                                    },
+                                }
+                            );
+                            const StickersPermsRole = message.guild.roles.cache.find(role => role.name === "StickersPerms");
+                            let embed2 = new Discord.MessageEmbed()
+                                .setColor('#554846')
+                                .setTitle("You've been given the StickerPerms role!")
+                                .setDescription("Thank you for purchasing the StickerPermsRole from the Realm shop!")
+                            await reaction.message.guild.members.cache.get(user.id).roles.add(StickersPermsRole);
+                            await reaction.message.author.send(embed2)
+                            
+
+                        }else{
+                            let embed3 = new Discord.MessageEmbed()
+                                .setColor('#554846')
+                                .setTitle("Error while purchasing the StickerPermsRole:")
+                                .setDescription("Not enough coins in wallet.")
+                            return message.author.send(embed3)
+                        }
                     }
-                    if (reaction.emoji.name === item2) {
+                    if (reaction.emoji.name === EmojiPermsEmoji) {
                         await reaction.message.author.send('1')
                     }
                   
