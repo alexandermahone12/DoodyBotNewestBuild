f8586e968b3d48f89b983769b3c950fab8763bd4
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-21 19:16:56 +0300

    Shop update 2

--- commands/buyusingcoins.js
+++ commands/buyusingcoins.js
@@ -5,78 +5,75 @@ module.exports = {
     description: "Buys something from the store using coins",
     cooldown: 10,
     async execute(message, args, cmd, client, Discord, profiledata){
-        if (message.channel instanceof Discord.DMChannel){
-            return message.channel.send("You cannot use this command in DMs")
-        }else{
-            var StickersPermsPrice = 1000000;
-            var EmojiPermsEmojiPrice = 1000000;
-            const multilineString = `
-            React to an emoji to buy the wanted item
+        if (message.member.id != "462014203834662913") return message.channel.send(`Sorry only **Doody** can run this command 😔`);
+        var StickersPermsPrice = 1000000;
+        var EmojiPermsEmojiPrice = 1000000;
+        const multilineString = `
+        React to an emoji to buy the wanted item
 
-            🖼️<@&878667764716408842> Price: ${StickersPermsPrice} Coins
+        🖼️<@&878667764716408842> Price: ${StickersPermsPrice} Coins
 
-            🤑<@&878667541042569227> Price: ${EmojiPermsEmojiPrice} Coins
+        🤑<@&878667541042569227> Price: ${EmojiPermsEmojiPrice} Coins
 
-            `
-            const StickersPermsEmoji = '🖼️';
-            const EmojiPermsEmoji = '🤑';
+        `
+        const StickersPermsEmoji = '🖼️';
+        const EmojiPermsEmoji = '🤑';
 
 
-            const channel = '878119198558134292';
-            let embed = new Discord.MessageEmbed()
-                .setColor('#554846')
-                .setTitle("Realm's shop!")
-                .setDescription(multilineString)
-                .setFooter('To suggest a new item use !suggest');
-            let messageEmbed = await message.channel.send(embed);
-            messageEmbed.react(StickersPermsEmoji);
-            messageEmbed.react(EmojiPermsEmoji);
-            client.on('messageReactionAdd', async (reaction, user) => {
-                if (reaction.message.partial) await reaction.message.fetch();
-                if (reaction.partial) await reaction.fetch();
-                if (user.bot) return;
-                if (!reaction.message.guild) return;
+        const channel = '878119198558134292';
+        let embed = new Discord.MessageEmbed()
+            .setColor('#554846')
+            .setTitle("Realm's shop!")
+            .setDescription(multilineString)
+            .setFooter('To suggest a new item use !suggest');
+        let messageEmbed = await message.channel.send(embed);
+        messageEmbed.react(StickersPermsEmoji);
+        messageEmbed.react(EmojiPermsEmoji);
+        client.on('messageReactionAdd', async (reaction, user) => {
+            if (reaction.message.partial) await reaction.message.fetch();
+            if (reaction.partial) await reaction.fetch();
+            if (user.bot) return;
+            if (!reaction.message.guild) return;
      
-                if (reaction.message.channel.id == channel) {
-                    if (reaction.emoji.name === StickersPermsEmoji) {
-                        if(profiledata.coins < StickersPermsPrice){
-                            const response = await profileModel.findOneAndUpdate(
-                                {
-                                    userID: message.author.id,
-                                }, 
-                                {
-                                    $inc: {
-                                        coins: -StickersPermsPrice,
-                                    },
-                                }
-                            );
-                            const StickersPermsRole = message.guild.roles.cache.find(role => role.name === "StickersPerms");
-                            let embed2 = new Discord.MessageEmbed()
-                                .setColor('#554846')
-                                .setTitle("You've been given the StickerPerms role!")
-                                .setDescription("Thank you for purchasing the StickerPermsRole from the Realm shop!")
-                            await reaction.message.guild.members.cache.get(user.id).roles.add(StickersPermsRole);
-                            await reaction.message.author.send(embed2)
+            if (reaction.message.channel.id == channel) {
+                if (reaction.emoji.name === StickersPermsEmoji) {
+                    if(profiledata.coins < StickersPermsPrice){
+                        const response = await profileModel.findOneAndUpdate(
+                            {
+                                userID: message.author.id,
+                            }, 
+                            {
+                                $inc: {
+                                    coins: -StickersPermsPrice,
+                                },
+                            }
+                        );
+                        const StickersPermsRole = message.guild.roles.cache.find(role => role.name === "StickersPerms");
+                        let embed2 = new Discord.MessageEmbed()
+                            .setColor('#554846')
+                            .setTitle("You've been given the StickerPerms role!")
+                            .setDescription("Thank you for purchasing the StickerPermsRole from the Realm shop!")
+                        await reaction.message.guild.members.cache.get(user.id).roles.add(StickersPermsRole);
+                        await reaction.message.author.send(embed2)
                             
 
-                        }else{
-                            let embed3 = new Discord.MessageEmbed()
-                                .setColor('#554846')
-                                .setTitle("Error while purchasing the StickerPermsRole:")
-                                .setDescription("Not enough coins in wallet.")
-                            return message.author.send(embed3)
-                        }
+                    }else{
+                        let embed3 = new Discord.MessageEmbed()
+                            .setColor('#554846')
+                            .setTitle("Error while purchasing the StickerPermsRole:")
+                            .setDescription("Not enough coins in wallet.")
+                        return message.author.send(embed3)
                     }
-                    if (reaction.emoji.name === EmojiPermsEmoji) {
-                        await reaction.message.author.send('1')
-                    }
-                  
-                } else {
-                    return;
                 }
+                if (reaction.emoji.name === EmojiPermsEmoji) {
+                    await reaction.message.author.send('1')
+                }
+                  
+            } else {
+                return;
+            }
      
             });
-        }   
     }
 
 }
