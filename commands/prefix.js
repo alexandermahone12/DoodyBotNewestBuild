1af31b24dcce726e570a01441340b40f10c99141
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-22 10:16:16 +0300

    Prefix command done

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,21 +1,32 @@
-
+const fs = require('fs')
 module.exports = {
 
     name: 'prefix',
     description: "creates an invite for the bot",
     execute(message, args, cmd, client, Discord, profiledata, prefix) {
-        if (!args.length){
-            message.channel.send("What would you like to change the prefix to?")
-            return
-        }
-        const changed = args[0]
-        prefix = changed
-        const commandsEmbed = new Discord.MessageEmbed()
-        .setColor('#554846')
-        .setTitle(`Prefix changed to ${prefix}`)
-        .setFooter('DoodyBot version 2', 'https://i.postimg.cc/zftSmvqt/shadow.png');
-        message.channel.send(commandsEmbed);
+        if (message.channel instanceof Discord.DMChannel){
+            return message.channel.send("You cannot use this command in DMs")
+        }else{
+            if(message.member.permissions.has("ADMINISTRATOR")){
+                if(!args.length) return message.channel.send("what would you like to change the prefix to?")
 
+                let prefixes = JSON.parse(fs.readFileSync('././prefixes.json', "utf-8"));
+                prefixes[message.guild.id] = {
+                    prefixes: args[0]
+                };
+
+                fs.writeFile("././prefixes.json", JSON.stringify(prefixes),(err) => {
+                    if (err) console.log(err)
+                });
+
+                const commandsEmbed = new Discord.MessageEmbed()
+                .setColor('#554846')
+                .setTitle(`Prefix changed to ${args[0]}`)
+                .setFooter('DoodyBot version 2', 'https://i.postimg.cc/zftSmvqt/shadow.png');
+                message.channel.send(commandsEmbed);
+    }else{
+        message.channel.send("you dont have the required permissions to change the prefix")
+    } }
     }
  
 }
