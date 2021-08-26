980ed646cba15d52ca8277b97d986332cde9f7f3
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-08-26 14:53:09 +0300

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -10,7 +10,6 @@ module.exports = {
         let avatar = mention.displayAvatarURL({dynamic : true, size: 1024})
 
         // Get joined date for member        
-        let roleCount = mention.guild.roles.size()
         let joinDate = await moment(mention.joinedTimestamp).format('MMMM Do YYYY, HH:mm:ss');
         // Get user account create date
         let createDate = await moment(mention.createdAt).format('MMMM Do YYYY, HH:mm:ss');   
@@ -22,12 +21,11 @@ module.exports = {
             { name: `Their username and tag:`, value: `${mention.tag}`, inline: true },
             { name: `They created Their account at:`, value: "`"+ `${createDate}` + "`", inline: true },
             { name: `They joined **${message.guild.name}** at:`, value: "`"+ `${joinDate}` + "`", inline: true },
-            { name: `They have:`, value: "`"+ `${roleCount} roles` + "`", inline: true },
         )
         .setImage(avatar)
         const download_button = new MessageButton()
             .setStyle("url")
-            .setLabel(`Download ${mention.displayName}'s avatar`)
+            .setLabel(`Download ${mention.username}'s avatar`)
             .setURL(avatar)
         message.channel.send(embed, download_button);
     }
