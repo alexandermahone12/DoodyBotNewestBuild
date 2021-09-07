423873c73bde52f28961b9f6485cf6083626dcd3
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-09-07 13:10:59 +0300

    Removed 2 packages

--- commands/status.js
+++ commands/status.js
@@ -6,10 +6,12 @@ module.exports = {
     cooldown: 5,
     execute(message, args, cmd, client, Discord, profiledata, commonjson){
         const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-        const reason = 'development'
+        const reason = 'server issues'
         const commandsEmbed1 = new Discord.MessageEmbed()
         .setColor(commonjson.defaultcolor)
+        .setTitle("DoodyBot status")
         .setDescription(`Last recorded downtime was ${duration} ago! Reason: ${reason}`)
+        .setFooter('DoodyBot version 2', 'https://i.postimg.cc/zftSmvqt/shadow.png')
         message.channel.send(commandsEmbed1)
         
     }
--- commands/tiktok.js
+++ commands/tiktok.js
@@ -1,33 +1,13 @@
-const TikTokScraper = require('tiktok-scraper')
-const number = require('easy-number-formatter')
 module.exports = {
     name: 'tiktok',
     description: 'searches for a tiktok account and returns the corresponding  information',
     cooldown: 5,
-    async execute(message, args, cmd, client, Discord, profiledata, commonjson) {
-            const user = await TikTokScraper.getUserProfileInfo(args[0]);
-            const userbe = new Discord.MessageEmbed()
-            .setColor(commonjson.defaultcolor)
-            if(user.user.verified == true){
-                userbe.setTitle(`User Info - @${user.user.uniqueId} <:Ver:750173291075076197>`)
-            }
-            else{
-                userbe.setTitle(`User Info - @${user.user.uniqueId}`)
-            }
-            userbe.setURL(`https://www.tiktok.com/@${user.user.uniqueId}`)
-            .setThumbnail(user.user.avatarThumb)
-            .addField("Username", `${user.user.uniqueId}`, true)
-            .addField("Nickname", `${user.user.nickname}`, true)
-            if(user.user.signature == ''){
-                userbe.addField("Bio", `No bio yet.`)
-            }
-            else{
-                userbe.addField("Bio", `${user.user.signature}`)
-            }
-            userbe.addField("Followers", number.formatNumber(`${user.stats.followerCount}`),true)
-            .addField("Following", number.formatNumber(`${user.stats.followingCount}`),true)
-            .addField("Hearts", number.formatNumber(`${user.stats.heartCount}`),true)
-            message.channel.send({embed: userbe })
+    execute(message, args, cmd, client, Discord, profiledata, commonjson) {
+        const commandsEmbed1 = new Discord.MessageEmbed()
+        .setColor(commonjson.defaultcolor)
+        .setTitle('command under development.')
+        message.channel.send(commandsEmbed1)
+        return
     
     
     }
