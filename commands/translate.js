f54e4b7bcd28ef3539b05fe2667d0fe8e31dc8ba
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-09-09 20:56:37 +0300

    Translate2

--- commands/translate.js
+++ commands/translate.js
@@ -1,22 +1,24 @@
-const translate = require('translate-google');
+const translate = require('google-translate');
 module.exports = {
     name: 'translate',
     description: 'NULL',
     cooldown: 3,
-    execute(message, args, cmd, client, Discord, profiledata, commonjson) {
-        translate(args.join(" "), {to : 'en'}).then(res => {
-            const embed = new Discord.MessageEmbed()
-            .setColor(commonjson.defaultcolor)
-            .setTitle("Translate to english")
-            .setDescription(`Translated: ${res}`)
-            message.channel.send(embed)
-        }).catch(err => {
-            const embed2 = new Discord.MessageEmbed()
-            .setColor(commonjson.failcolor)
-            .setTitle("Error")
-            .setDescription(`Error: ${err}`)
-            message.channel.send(embed2)
-            console.log(err)
-        })
+    async execute(message, args, cmd, client, Discord, profiledata, commonjson) {
+        //EMBED
+        const embed = new Discord.MessageEmbed
+        .setColor(commonjson.failcolor)
+        .setTitle("Error")
+        .setDescription("You cant translate nothing idiot.")
+
+        const query = args.join(' ');
+        if(!query) return message.channel.send(embed);
+
+        const translate = await translate(query, { to: 'en' });
+        const embed2 = new Discord.MessageEmbed
+        .setColor(commonjson.defaultcolor)
+        .setTitle(`Translate **${query}** to english`)
+        .setDescription(`Translation: ${translate}`)
+        message.channel.send(embed2);
+        
     }
 }
