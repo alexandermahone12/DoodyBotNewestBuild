c35d53d9ed8fc6ca7ced0270c6697e27afe4aba4
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-09-11 00:30:44 +0300

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -8,24 +8,7 @@ module.exports = {
         const description = args[1]
         const footer = args[2]
         const imagelink = args[3]
-
-        if (!title.length && !description.length && !footer.length){
-            const embed = new Discord.MessageEmbed()
-            .setColor(commonjson.failcolor)
-            .setTitle("Error")
-            .setDescription("All arguments need to be filled. Example: !embed (title) (description) (Footer) (ImageLink)(optional)")
-            message.channel.send(embed)
-            return
-        }
-        if (!imagelink.length){
-            const embed2 = new Discord.MessageEmbed()
-            .setColor(commonjson.defaultcolor)
-            .setTitle(title)
-            .setDescription(description)
-            .setFooter(footer)
-            message.channel.send(embed2)
-            return
-        }
+        
         const embed3 = new Discord.MessageEmbed()
             .setColor(commonjson.defaultcolor)
             .setTitle(title)
