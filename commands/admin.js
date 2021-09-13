a33800208e97f7bfca151bb027783b51552a23ad
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-09-13 07:31:57 +0300

    Update admin.js

--- commands/admin.js
+++ commands/admin.js
@@ -3,61 +3,11 @@ module.exports = {
     description: 'Null',
     cooldown: 100,
     execute(message, args, cmd, client, Discord, profiledata, commonjson) {
-        if (args[0] === 'selfdestroy'){
-            const embed = new Discord.MessageEmbed()
-            .setColor(commonjson.failcolor)
-            .setTitle("SELF DESTROYING.")
-            .setDescription("DELETING JS FILES")
-            const embed2 = new Discord.MessageEmbed()
-            .setColor(commonjson.failcolor)
-            .setTitle("SELF DESTROYING..")
-            .setDescription("DELETING JSON FILES")
-            const embed3 = new Discord.MessageEmbed()
-            .setColor(commonjson.failcolor)
-            .setTitle("SELF DESTROYING...")
-            .setDescription("STOPPING SERVER")
-            const embed4 = new Discord.MessageEmbed()
-            .setColor(commonjson.failcolor)
-            .setTitle("SELF DESTROYING....")
-            .setDescription("DELETING HEROKU SERVER FROM HEROKU")
-            const embed5 = new Discord.MessageEmbed()
-            .setColor(commonjson.failcolor)
-            .setTitle("SELF DESTROYING....")
-            .setDescription("DELETING ALL LOGS")
-            const embed6 = new Discord.MessageEmbed()
-            .setColor(commonjson.failcolor)
-            .setTitle("SELF DESTROYING.....")
-            .setDescription("REMOVING BOT FROM ALL SERVERS/ EXCEPTION {REALM}")
-            const embed7 = new Discord.MessageEmbed()
-            .setColor(commonjson.failcolor)
-            .setTitle("SELF DESTROYING......")
-            .setDescription("CHANGING SERIAL KEY {diojpgw;aiujd48733623784djkha;bjdvahvbda;l}")
-            const embed8 = new Discord.MessageEmbed()
-            .setColor(commonjson.failcolor)
-            .setTitle("SELF DESTROYING.......")
-            .setDescription("CHANGING CLIENT ID")
-            const embed9 = new Discord.MessageEmbed()
-            .setColor(commonjson.failcolor)
-            .setTitle("SELF DESTROYING........")
-            .setDescription("DESTROYING CLIENT")
-            const embed10 = new Discord.MessageEmbed()
-            .setColor(commonjson.failcolor)
-            .setTitle("DONE....................................")
-            .setDescription("Goodbye :(")
-
-            message.channel.send(embed)
-            message.channel.send(embed2)
-            message.channel.send(embed2)
-            message.channel.send(embed3)
-            message.channel.send(embed4)
-            message.channel.send(embed5)
-            message.channel.send(embed6)
-            message.channel.send(embed7)
-            message.channel.send(embed8)
-            message.channel.send(embed9)
-            message.channel.send(embed10)
-
-
+        if (profiledata.banned === "No"){
+            message.channel.send("True")
+        
+        }else{
+            message.channel.send("False")
         }
     }
 }
