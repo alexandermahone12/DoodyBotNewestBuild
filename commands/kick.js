502bad75967e605f0ba80fad26b575bb6ad48d74
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-09-13 12:03:48 +0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -5,7 +5,7 @@ module.exports = {
     cooldown: 5,
     async execute(message, args, cmd, client, Discord, profiledata, commonjson) {
 
-        const target = message.mentions.users.first() || client.users.cache.get(args[0]);
+        const target = message.mentions.members.first()
         if (!target) {
             const embed1 = new Discord.MessageEmbed()
             .setColor(commonjson.failcolor)
