caffa150adf7d88cce2f705227e1caaa7deb8a9b
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-09-15 00:30:15 +0300

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -44,7 +44,7 @@ module.exports = async (Discord, client, message) => {
         };
     }
     let prefix = prefixes[message.guild.id].prefixes;
-    console.log(`${message.author.tag} sent a message`);
+    console.log(`${message.author.tag} sent a message in server (${message.guild}) message was (${message.content}) at time ${time.Date.now()}`);
     
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
