43c68aa459f850a685abeac630e857c28c254355
alexandermahone12 <84809587+alexandermahone12@users.noreply.github.com>
2021-09-20 01:44:28 +0300

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -44,7 +44,7 @@ module.exports = async (Discord, client, message) => {
         };
     }
     let prefix = prefixes[message.guild.id].prefixes;
-
+    if (!message.content.startsWith(prefix) || message.author.bot) return
 
     let profiledata;
     try{
